e33a887f4b7dbe1793b642fd1b362001cf395e4b
C.J. Collier <cjcollier@linuxfoundation.org>
2017-03-27 13:19:05 -0700

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: I0a46b2b7f6cb7effea3a1eed12a89ef622d385d2
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- Platform/BridgeRT/BridgeLog.h
+++ Platform/BridgeRT/BridgeLog.h
@@ -1,17 +1,27 @@
 //
-// Copyright (c) 2015, Microsoft Corporation
-//
-// Permission to use, copy, modify, and/or distribute this software for any
-// purpose with or without fee is hereby granted, provided that the above
-// copyright notice and this permission notice appear in all copies.
-//
-// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
-// IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// 
+// IN Copyright (c) %s, Microsoft Corporation
+// IN 
+// IN SPDX-License-Identifier: Apache-2.0 *
+// IN 
+// IN All rights reserved. This program and the accompanying materials are
+// IN made available under the terms of the Apache License, Version 2.0
+// IN which accompanies this distribution, and is available at
+// IN http://www.apache.org/licenses/LICENSE-2.0
+// IN 
+// IN Permission to use, copy, modify, and/or distribute this software for
+// IN any purpose with or without fee is hereby granted, provided that the
+// IN above copyright notice and this permission notice appear in all
+// IN copies.
+// IN 
+// IN THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+// IN WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// IN WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+// IN AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+// IN DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+// IN PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+// IN TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// IN PERFORMANCE OF THIS SOFTWARE.
 //
 
 #pragma once
@@ -45,4 +55,3 @@ private:
     void Shutdown(void);
     volatile long m_shutdown;
 };
-
